796af26449f8d0156e9f174ea026357cfe998f22
roxsal <rohit04saluja@protonmail.com>
2015-11-27 22:31:07 +0530

    Testing complete and bugs fixed for CryptArithmatic.pl, Update to description README.md

--- prolog/AI/CryptArithmatic.pl
+++ prolog/AI/CryptArithmatic.pl
@@ -6,28 +6,27 @@ sum(N1, N2, N) :-
 sum1([], [], [], C, C, D, D).
 % recursive call will exit with empty lists
 
-sum1([D1|N1], [D2|N2], [D|N], Cr, C, Digs1, Digs) :-
-	sum1(N1, N2, N, Cr, Cl, Digs1, Digs2),
-	digsum(D1, D2, Cl, D, C, Digs2, Digs).
+sum1([D1|N1], [D2|N2], [D|N], C_in, C_out, DigsA, DigsR):-
+	sum1(N1, N2, N, C_in, C, DigsA, Digs),
+	digsum(D1, D2, C, D, C_out, Digs, DigsR).
 % recursive calls for the remaining list
 % digsum handles addition of 2 letters at a time
 
-digsum(D1, D2, C1, D, C, Digs1, Digs) :-
-	del_var(D1, Digs1, Digs2),
-	del_var(D2, Digs2, Digs3),
-	del_var(D, Digs3, Digs),
-	S is D1 + D2 + C1,
-	D is S mod 10,	% remainder of division
-	C is S // 10.	% integer of division
-% D1+D2+C1=D+C
+digsum(D1, D2, C_in, D, C_out, DigsA, DigsR) :-
+	del_var(D1, DigsA, Digs1),
+	del_var(D2, Digs1, Digs2),
+	del_var(D, Digs2, DigsR),
+	S is D1 + D2 + C_in,
+	D is S mod 10,		% remainder of division
+	C_out is S // 10.	% integer of division
+% D1+D2+C_in=D+C_out
 
 del_var(A, L, L) :-
 	nonvar(A), !.	% A already instantiated
 
 del_var(A, [A|L], L).
+% Assign head of the list to variable
 
-def_var(A, [B|L], [B|L1]) :-
+del_var(A, [B|L], [B|L1]) :-
 	del_var(A, L, L1).
-
-puzzle1([0,S,E,N,D], [0,M,O,R,E], [M,O,N,E,Y]).
-% Examples of problems
+% Strip the head of the list and re-assign
---
+++ prolog/AI/README.md
@@ -0,0 +1,5 @@
+# CryptArithmatic Problem <CryptArithmatic.pl>
+- Prolog program to solve a cryptarithimatic problem
+- default available digits are 0 to 9
+- Example for usage:
+?- sum([0,S,E,N,D], [0,M,O,R,E], [M,0,N,E,Y])
