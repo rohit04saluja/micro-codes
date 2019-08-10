73e60617bbbd5d7ba7aac933e78ab47d11d824f5
Rohit Saluja <r4saluja@gmail.com>
2019-08-10 11:01:30 +0530

    Declaring data struct for the queue

--- c++/Data Structures/ArrayQueue.hpp
+++ c++/Data Structures/ArrayQueue.hpp
@@ -1,3 +1,29 @@
 /*
  * Here is class of QUEUE using array
  */
+#include <stdio>
+#include <system_error>
+namespace mysnippets
+{
+  /* Class for QUEUE using array */
+  class ArrayQueue{
+  private:
+    // TODO: data type obnoxious
+    /* Internal storage (array) for queue */
+    int *a;
+  protected:
+    /* Anchors for queue operations */
+    int front = 0, rear = 0;
+  public:
+    ArrayQueue (const int size);
+    ~ArrayQueue (void);
+
+    /* Enqueue and dequeue operation for the queue */
+    void enqueue (const int data);
+    int dequeue (void);
+
+    /* Auxilary useful operation for the queue */
+    bool isEmpty (void);
+    bool isFull (void);
+  }
+}
